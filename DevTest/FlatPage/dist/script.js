5ea26d36a151ea310759c599dbcf57dab27bc085
DackVader <dackvader@outlook.com>
2023-05-26 16:00:20 +0700

    change name

--- DevTest/FlatPage/dist/script.js
+++ DevTest/FlatPage/dist/script.js
@@ -10,3 +10,14 @@ var ele, eleIndex,  moveLeft;
             marginLeft: moveLeft
         }, 600);
  });
+
+ $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
